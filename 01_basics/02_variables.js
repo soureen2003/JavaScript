7a6e4d63e80fbed39bef52b1d6614bff37e94b1a
SOUREEN LAHA <105166164+soureen2003@users.noreply.github.com>
2024-09-24 12:30:14 +0000

    datatypes

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,11 @@
+"use strict" //treat all js code as newer version
+ //alert("") this will not execute as we are using node js, not browser
+
+let age = 3
+ console.log(typeof age);// this will print number 
+
+console.log(typeof null); // this will print object so the tye of null is object
+console.log(typeof undefined); // undefined
+
+
+ 
---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 14453
+
+let accountEmail = "soureen@google.com"
+
+var accountPassword = "1614112"
+
+
+//var is not used anymore because of issue in block scope and functional scope
+
+city = "Kolkata"
+//sometimes we dont need to declare the type of variable. prefer not to use
+console.log(city);
+console.table([accountId,accountEmail,accountPassword,city])
