e6adaf4a5c7d9d561c4c35fbafc200236c0cbbda
SOUREEN LAHA <105166164+soureen2003@users.noreply.github.com>
2024-09-24 18:10:23 +0530

    Update 02_variables.js

--- 01_basics/02_variables.js
+++ 01_basics/02_variables.js
@@ -6,6 +6,6 @@ let age = 3
 
 console.log(typeof null); // this will print object so the tye of null is object
 console.log(typeof undefined); // undefined
-
+console.log(typeof Symbol); //function
 
  
