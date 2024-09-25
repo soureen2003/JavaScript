dfea664fe07f46f9727920fde844db43f8b2b15d
SOUREEN LAHA <105166164+soureen2003@users.noreply.github.com>
2024-09-25 12:35:40 +0000

    comparision of variables done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,12 @@
+console.log("2" > 1);
+console.log("01" > 2);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+//strict conversion
+//in this case we also check the types of the variables
+
+
+console.log("2" === 2);
