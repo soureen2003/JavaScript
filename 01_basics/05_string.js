19999204bebd69a3d7c16818bae23d71c21caa9a
SOUREEN LAHA <105166164+soureen2003@users.noreply.github.com>
2024-09-26 15:15:40 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,14 @@
+const name = "Soureen"
+const repoCount = 4
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+//this is called string interpoletion
+
+const gameName = new String("Omen")
+console.log(gameName);
+console.log(gameName.length);
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('O'));
+
+
