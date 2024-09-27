8319480f2312778a9213f0c206a96d1fa3eafe30
SOUREEN LAHA <105166164+soureen2003@users.noreply.github.com>
2024-09-27 19:25:01 +0000

    strings done

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -4,11 +4,23 @@ const repoCount = 4
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 //this is called string interpoletion
 
-const gameName = new String("Omen")
+const gameName = new String("Omen-Sage")
 console.log(gameName);
 console.log(gameName.length);
 
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('O'));
+const newString = gameName.substring(0,4)
+console.log(newString);
 
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString)
+
+const myName = "  Soureen  "
+console.log(myName);
+console.log(myName.trim());
+
+const url = "https://soureen.com/soureen%20laha"
+console.log(url.replace('%20','-'));
 
