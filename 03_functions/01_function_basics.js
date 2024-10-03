51b995021701a2261d1047041a8defa5a9e8a157
SOUREEN LAHA <105166164+soureen2003@users.noreply.github.com>
2024-10-03 09:27:59 +0000

    function done

---
+++ 03_functions/01_function_basics.js
@@ -0,0 +1,15 @@
+function myFunction(){
+    console.log("ARO EKBAR CHOLO FIRE JAI");
+    
+}
+
+myFunction();
+
+//arrow function
+
+const sum = (a,b)=>{
+    console.log(a+b);
+    
+}
+sum(1,2)
+console.log(sum);
